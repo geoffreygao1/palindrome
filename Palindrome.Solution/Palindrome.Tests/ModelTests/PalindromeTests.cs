77e2c6af00c33544b7c3104ebbe537fca3cb035e
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-31 19:25:44 -0800

    split array
    Co-authored by: Noah Cowan <nsncowan@gmail.com>

--- Palindrome.Solution/Palindrome.Tests/ModelTests/PalindromeTests.cs
+++ Palindrome.Solution/Palindrome.Tests/ModelTests/PalindromeTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName;
+using Palindrome;
 
-namespace ProjectName.Tests
+namespace Palindrome.Models
 {
   [TestClass]
   public class ClassNameTests
@@ -13,5 +13,25 @@ namespace ProjectName.Tests
     //   // any necessary logic to prep for test; instantiating new classes, etc.
     //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
     // }
+
+    [TestMethod]
+    public void Palindrome_AssignsAttribute_InputString()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Palindrome test = new Palindrome("test");
+      Assert.AreEqual("test", test.Input);
+    }
+
+    [TestMethod]
+    public void Palindrome_SplitsInputToArray_Array()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Palindrome test = new Palindrome("test");
+      char[] testArray = { 't', 'e', 's', 't' };
+      Assert.AreEqual(testArray[0], test.SplitWord()[0]);
+      Assert.AreEqual(testArray[1], test.SplitWord()[1]);
+      Assert.AreEqual(testArray[2], test.SplitWord()[2]);
+      Assert.AreEqual(testArray[3], test.SplitWord()[3]);
+    }
   }
 }
